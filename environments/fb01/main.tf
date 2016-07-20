b28097918c7088563636f062a0723162c3e9cd43
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2016-07-20 14:45:10 +0100

    AMI id update to match the region

--- environments/fb01/main.tf
+++ environments/fb01/main.tf
@@ -18,6 +18,6 @@ module env {
   publishing_subnet_cidr_block        = ["10.10.10.0/24", "10.10.11.0/24", "10.10.12.0/24"]
   publishing_subnet_tags              = "${var.publishing_subnet_tags}"
   publishing_subnet_availability_zone = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  publishing_ami_id                   = "ami-408c7f28"
+  publishing_ami_id                   = "ami-f9dd458a"
   publishing_instance_type            = "t1.micro"
 }
